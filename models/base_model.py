c9e3e8e20f567fa5aaac4c59cebfb076f956e05a
David Onuta <ogagaonuta@gmail.com>
2023-02-12 21:07:26 +0000

    Create class that defines all common objects for other classes

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Create class BaseModel that defines all
+common attributes/methods for other classes"""
+
+
+import datetime as dt
+import json
+import uuid
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initializes an instance of the BaseModel object"""
+        self.id = str(uuid.uuid4())
+        self.created_at = dt.datetime.today()
+        self.updated_at = dt.datetime.today()
+
+    def __str__(self):
+        """Returns an informal string representation"""
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, str(self.__dict__)))
+
+    def save(self):
+        """Updates attribute updated_at with the current datetime"""
+        self.updated_at = dt.datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance"""
+        copy_dict = dict(self.__dict__) # Create copy of self.__dict__
+        copy_dict["created_at"] = copy_dict["created_at"].isoformat()
+        copy_dict["updated_at"] = copy_dict["updated_at"].isoformat()
+        copy_dict["__class__"] = type(self).__name__
+        return (copy_dict)
