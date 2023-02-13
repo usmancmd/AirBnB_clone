69578a0efc305e5b64c050c37ca91bbba201c087
David Onuta <ogagaonuta@gmail.com>
2023-02-13 22:49:03 +0000

    Implement kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,15 +13,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initializes an instance of the BaseModel object"""
-        self.id = str(uuid.uuid4())
-        self.created_at = dt.datetime.today()
-        self.updated_at = dt.datetime.today()
+        if (not kwargs):
+            self.id = str(uuid.uuid4())
+            self.created_at = dt.datetime.today()
+            self.updated_at = dt.datetime.today()
 
-        if (kwargs is not None):
+        else:
             for key, value in kwargs.items():
-                if ((key == created_at) or (key == updated_at)):
-                    self.__dict__[key] = dt.fromisoformat(value)
-                    self.__dict__[key] = dt.fromisoformat(value)
+                if ((key == "created_at") or (key == "updated_at")):
+                    self.__dict__[key] = dt.datetime.fromisoformat(value)
+                    self.__dict__[key] = dt.datetime.fromisoformat(value)
                 else:
                     self.__dict__[key] = value
 
